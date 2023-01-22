9107031632987de96965fee2eaa5507121862551
Sumit Kumar Bera <61411787+Sumit-bera@users.noreply.github.com>
2023-01-23 01:55:34 +0530

    Delete Java 8 Features directory

--- Java 8 Features/Introduction to Nashorn/Assignment01/Sample.js
+++
@@ -1,6 +0,0 @@
-/*Write a JavaScript code which displays the current system date and time.
-Save the code as Sample.js, invoke it using jjs tool and display the result.
-*/
-
-var today = new Date();
-print(today);
